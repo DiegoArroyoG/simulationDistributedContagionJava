dd9fc01ea7911bdd9b0549f42ddf225c74fdb9df
David Vanegas <david-vanegas@javeriana.edu.co>
2020-04-09 21:55:05 -0500

    Commit main - pais

--- broker/Main.java
+++ broker/Main.java
@@ -1,8 +1,47 @@
+import java.io.File;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 
-public class Main{
-    public static void main(String[] args) {
-        Broker broker = new Broker();
+public class Main {
 
+    private static String[] line;
+
+    public static void main(String[] args) throws UnknownHostException {
+        Inet4Address dir_origen = null , dir_destino = null;
+        int port = 0;
+        List <Pais> paises_broker = leerFichero();
+
+        if(args.length < 3){
+            dir_origen =  (Inet4Address) Inet4Address.getByName(args[0]);
+            port = Integer.parseInt(args[1]);
+            dir_destino = (Inet4Address) Inet4Address.getByName(args[2]);
+        }
+
+        Broker bro = new Broker(dir_origen, port, paises_broker);
+
+        for(Pais a : paises_broker){
+            a.setBroker(bro);
+        }
+
+        bro.check_in();
+    }
+
+    private static List<Pais> leerFichero() {
+        List <Pais> paises_broker = new ArrayList <Pais> (); 
+        try {
+            Scanner input = new Scanner(new File("Broker/Config.txt"));
+            while (input.hasNextLine()) {
+                line = input.nextLine().split(",");
+                Pais p1 = new Pais(line[0].trim(), line[1].trim(), line[2].trim(), line[3].trim(), line[4].trim(), line[5].trim());
+                paises_broker.add(p1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return paises_broker;
     }
 }
--- pais/Pais.java
+++ pais/Pais.java
@@ -1,10 +1,31 @@
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.util.List;
 
+public class Pais {
+    private String nombre;
+    private int poblacion;
+    private int infectados;
+    private int recuperados;
+    private int vulnerable;
+    private int beta;
+    private int gamma;
+    private Inet4Address dir_ip;
+    private List<Pais> vecinos;
+    private Broker broker_mine;
 
+    public Pais(String nombre, String poblacion, String infectados, String pAis, String g, String ip)
+            throws UnknownHostException {
+        this.poblacion = Integer.parseInt(poblacion);
+        this.infectados = Integer.parseInt(infectados);
+        this.recuperados = 0;
+        this.vulnerable = Integer.parseInt(poblacion) - Integer.parseInt(infectados);
+        this.beta = 1 - Integer.parseInt(pAis);
+        this.gamma = Integer.parseInt(g);
+        this.dir_ip = (Inet4Address) Inet4Address.getByName(ip);
+	}
 
-public class Pais
-{
-    public Pais()
-    {
-        
-    }
+	public void setBroker(Broker bro) {
+        this.broker_mine = bro;
+	}
 }
