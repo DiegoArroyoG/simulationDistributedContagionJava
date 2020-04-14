14cc89c4dfe9e6a4dc4c332500b658ad974a18d6
DiegoArroyoG <arroyodiego6@gmail.com>
2020-04-13 19:30:41 -0500

    Servidor y cliente pais

--- broker/Pais.java
+++ broker/Pais.java
@@ -4,28 +4,30 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.Inet4Address;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Pais extends Thread implements Serializable{
+public class Pais extends Thread implements Serializable {
 
         private String nombre;
         private int poblacion;
         private int peso;
         private int infectados;
         private int recuperados;
+        private int port;
         private float vulnerable;
         private float beta;
         private float gamma;
         private Inet4Address dir_ip;
         private Broker broker_mine;
-        private HashMap <String,Inet4Address> vecinos;
+        private HashMap<Integer, Inet4Address> vecinos;
 
-        public Pais(String nombre, String poblacion, String infectados, String pAis, String g, String ip, Map<String, Inet4Address> vecinos2)
-                        throws UnknownHostException {
+        public Pais(String nombre, String poblacion, String infectados, String pAis, String g, String ip,
+                        Map<Integer, Inet4Address> vecinos, int port) throws UnknownHostException {
                 this.nombre = nombre;
                 this.poblacion = Integer.parseInt(poblacion);
                 this.infectados = Integer.parseInt(infectados);
@@ -35,7 +37,8 @@ public class Pais extends Thread implements Serializable{
                 this.gamma = Float.parseFloat(g);
                 this.dir_ip = (Inet4Address) Inet4Address.getByName(ip);
                 this.peso = 0;
-                this.vecinos = (HashMap<String, Inet4Address>) vecinos2;
+                this.vecinos.putAll(vecinos);
+                this.port = port;
         }
 
         public void setBroker(Broker bro) {
@@ -62,13 +65,10 @@ public class Pais extends Thread implements Serializable{
                 this.peso = calculo;
         }
 
-        public void call(Inet4Address dir_destino, int valor) throws IOException, ClassNotFoundException
-        {
+        public void call(Inet4Address dir_destino, int port_destino) throws IOException, ClassNotFoundException {
                 Socket client = null;
                 try {
-                        client = new Socket(dir_destino, 8888);
-                        ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
-                        out.writeObject("Me contagie");
+                        client = new Socket(dir_destino, port_destino);
 
                 } catch (UnknownHostException e) {
                         System.out.println("Socket:" + e.getMessage());
@@ -78,7 +78,7 @@ public class Pais extends Thread implements Serializable{
                         System.out.println("readline:" + e.getMessage());
                 } finally {
                         if (client != null)
-                                try { 
+                                try {
                                         client.close();
                                 } catch (IOException e) {
                                         System.out.println("close:" + e.getMessage());
@@ -88,10 +88,37 @@ public class Pais extends Thread implements Serializable{
         }
 
         public void run() {
+                if (infectados == 0) {
+                        ServerSocket ListenSocket;
+                        try {
+                                ListenSocket = new ServerSocket(port);
+                                ListenSocket.accept();
+                                ListenSocket.close();
+                        } catch (IOException e1) {
+                                // TODO Auto-generated catch block
+                                e1.printStackTrace();
+                        }
+                }
+                infectados = 1;
+                System.out.println("XXXXXXXXXXXXXXXXXXX QUE COMIENCE EL JUEGO XXXXXXXXXXXXXXXXXXXXXXXXXXXx");
                 while (true) {
                         this.infectados = this.infectados + 1;
                         try {
-                                System.out.println(nombre + " " +this.peso*1000);
+                                System.out.println(nombre + " " + this.peso * 1000);
+                                if (infectados == poblacion * 0.5)
+                                        for (Map.Entry<Integer, Inet4Address> entry : vecinos.entrySet()) {
+                                                Integer port_destino = entry.getKey();
+                                                Inet4Address dir_destino = entry.getValue();
+                                                try {
+                                                        call(dir_destino, port_destino);
+                                                } catch (ClassNotFoundException e) {
+                                                        // TODO Auto-generated catch block
+                                                        e.printStackTrace();
+                                                } catch (IOException e) {
+                                                        // TODO Auto-generated catch block
+                                                        e.printStackTrace();
+                                                }
+                                        }
                                 sleep(this.peso * 1000);
                         } catch (InterruptedException e) {
                                 // TODO Auto-generated catch block
